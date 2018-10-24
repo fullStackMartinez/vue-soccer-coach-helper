dbbef19fd7f595078b0a437b607b64cf3b353082
Esteban Martinez <fullstackmartinez@gmail.com>
2018-10-24 13:18:14 -0600

    added empty string property in data, along with an array to hold input displaying items in array using v-for loop in a ul li tags

--- app/app.js
+++ app/app.js
@@ -4,15 +4,26 @@ const app = new Vue({
 	data: {
 		title: 'Hello',
 		newPlayer: '',
+		players: []
 	},
 	methods: {
 		addPlayer() {
-			console.log('form submitted');
+			this.players.push(this.newPlayer);
 		}
 	}
 });
 
 //going to create an instance for the practice to do list
 const toDo = new Vue({
-
+	el: '#',
+	data: {
+		title: 'Hello',
+		newPlayer: '',
+		players: []
+	},
+	methods: {  //here I want to push an object, not variable, with a property done: false
+		addPlayer() {
+			this.players.push(newPlayer);
+		}
+	}
 });
