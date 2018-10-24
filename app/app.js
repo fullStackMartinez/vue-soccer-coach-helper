f2fd7344f0f20d79a4af04709aebecc566686351
Esteban Martinez <fullstackmartinez@gmail.com>
2018-10-24 15:43:27 -0600

    added practice.html to make a drills list added button to remove player from roster added dynamic class to drills li to strike through when clicked

--- app/app.js
+++ app/app.js
@@ -2,28 +2,47 @@
 const app = new Vue({
 	el: '#app',
 	data: {
-		title: 'Hello',
+		title: 'Player Roster',
 		newPlayer: '',
-		players: []
+		players: [],
+		newPosition: '',
 	},
 	methods: {
 		addPlayer() {
-			this.players.push(this.newPlayer);
+			//pushing what the input value is into our empty array so we can loop over it
+			this.players.push({
+				name: this.newPlayer,
+				position: this.newPosition,
+				isOnTeam: false
+			});
+			//setting the variable to an empty string so that it clears out default input after every time we run method/click add player
+			this.newPlayer = '';
+			this.newPosition = '';
+		},
+
+		removePlayer(player) {
+			const playerArray = this.players.indexOf(player);
+
+			this.players.splice(playerArray, 1);
 		}
 	}
 });
 
 //going to create an instance for the practice to do list
 const toDo = new Vue({
-	el: '#',
+	el: '#practice',
 	data: {
-		title: 'Hello',
-		newPlayer: '',
-		players: []
+		title: 'Practice Planner',
+		newDrill: '',
+		drills: []
 	},
 	methods: {  //here I want to push an object, not variable, with a property done: false
-		addPlayer() {
-			this.players.push(newPlayer);
-		}
+		addDrill() {
+			this.drills.push({
+				drillName: this.newDrill,
+				completed: false
+			});
+			this.newDrill = '';
+		},
 	}
 });
